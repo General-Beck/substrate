703ddb11c9503b698d3b737077550cf8a12ad9e8
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2020-03-19 06:33:53 +0300

    update toml

--- bin/node/cli/bin/main.rs
+++ bin/node/cli/bin/main.rs
@@ -20,18 +20,19 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    loop {}
-}
-
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+#[cfg(target_os = "linux")]
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+	// this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    loop {}
+}
+
 
 #![warn(missing_docs)]
 
